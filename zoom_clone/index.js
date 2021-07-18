9568546e5a293ff628806a9397edb661cf378da2
Saranga99 <thathsarasaranga12345@gmail.com>
2021-07-18 22:36:18 +0530

    Update index.js

--- zoom_clone/index.js
+++ zoom_clone/index.js
@@ -16,4 +16,26 @@ app.get("/",(req,res)=>{
     res.send("Hi is running");
 })
 
+//socket.io
+io.on("connection",(socket)=>{
+    socket.emit("me",socket.id);
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("callended");
+ });
+
+ socket.on("calluser",({userToCall,signalData,from,name})=>{
+    io.to(userToCall).emit("calluser",{
+        signal:signalData,
+        from:from,
+        name:name,
+    });
+ });
+
+ socket.on("answeroncall",(data)=>{
+     io.to(data.to).emit("callaccepted",data.signal);
+ });
+
+});
+
 server.listen(PORT,()=>console.log(`Server is running on ${PORT}`));
